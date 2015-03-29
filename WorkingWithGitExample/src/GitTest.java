2b435ac420f1c0cfea3d166478d15fe5b5e7e92b
IdoSaroka <idos777@hotmail.co.il>
2015-03-29 20:17:21 +0300

    First change

--- WorkingWithGitExample/src/GitTest.java
+++ WorkingWithGitExample/src/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Lets see if we can do the same thing with git");
+		
+		System.out.println("Test 2");
 	
 	}
 
