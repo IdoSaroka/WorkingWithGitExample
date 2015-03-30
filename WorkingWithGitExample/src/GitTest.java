446e988db830a9992fbcf72e8f690217a684ebb0
shimonben <sbalul20@gmail.com>
2015-03-30 18:26:05 +0300

    muhahaa

--- WorkingWithGitExample/src/GitTest.java
+++ WorkingWithGitExample/src/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
 		System.out.println("Lets see if we can do the same thing with git");
 		System.out.println("Test 2");
 		System.out.println("kushkushim");
+		ido shoded zkenot
 	}
 
 }
